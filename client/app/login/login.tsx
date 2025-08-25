eba38dc7a62049125b3d472704fb6bca39c58758
최현준 <choijjunii1@gmail.com>
2025-08-25 09:46:05 +0900

    feat: 로그인 폴더 및 파일 생성

---
+++ client/app/login/login.tsx
@@ -0,0 +1,12 @@
+import { View, Text, TextInput,Button } from "react-native";
+
+export default function Login() {
+  return (
+    <View>
+      <Text>Login Screen</Text>
+      <TextInput></TextInput>
+      <Button></Button>
+    </View>
+
+  );
+}
